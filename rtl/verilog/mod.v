1894dac586dd63cf8762a6f3cc9e3e5c4ef216c8
hugang <hugang@hugang-macbook.(none)>
2009-09-17 15:14:10 +0800

    v4lx15 compress/uncompress

--- rtl/verilog/mod.v
+++ rtl/verilog/mod.v
@@ -46,9 +46,8 @@ module mod(/*AUTOARG*/
    pullup(m_dst_putn);
    pullup(m_src_getn);
    pullup(m_endn);
-   // synopsys translate_on
 
-   rabin64_fp rbhash(/*AUTOINST*/
+   read read(/*AUTOINST*/
 	     // Outputs
 	     .m_src_getn		(m_src_getn),
 	     .m_dst_putn		(m_dst_putn),
@@ -66,11 +65,104 @@ module mod(/*AUTOARG*/
 	     .m_src_empty		(m_src_empty),
 	     .m_dst_almost_full		(m_dst_almost_full),
 	     .m_dst_full		(m_dst_full));
+   fill fill(/*AUTOINST*/
+	     // Outputs
+	     .m_src_getn		(m_src_getn),
+	     .m_dst_putn		(m_dst_putn),
+	     .m_dst			(m_dst[63:0]),
+	     .m_dst_last		(m_dst_last),
+	     .m_endn			(m_endn),
+	     // Inputs
+	     .wb_clk_i			(wb_clk_i),
+	     .wb_rst_i			(wb_rst_i),
+	     .m_enable			(m_enable),
+	     .dc			(dc[23:0]),
+	     .m_src			(m_src[63:0]),
+	     .m_src_last		(m_src_last),
+	     .m_src_almost_empty	(m_src_almost_empty),
+	     .m_src_empty		(m_src_empty),
+	     .m_dst_almost_full		(m_dst_almost_full),
+	     .m_dst_full		(m_dst_full));
+   
+   copy copy(/*AUTOINST*/
+	     // Outputs
+	     .m_src_getn		(m_src_getn),
+	     .m_dst_putn		(m_dst_putn),
+	     .m_dst			(m_dst[63:0]),
+	     .m_dst_last		(m_dst_last),
+	     .m_endn			(m_endn),
+	     // Inputs
+	     .wb_clk_i			(wb_clk_i),
+	     .wb_rst_i			(wb_rst_i),
+	     .m_enable			(m_enable),
+	     .dc			(dc[23:0]),
+	     .m_src			(m_src[63:0]),
+	     .m_src_last		(m_src_last),
+	     .m_src_almost_empty	(m_src_almost_empty),
+	     .m_src_empty		(m_src_empty),
+	     .m_dst_almost_full		(m_dst_almost_full),
+	     .m_dst_full		(m_dst_full));
+   // synopsys translate_on
 
-   assign 	 m_cap = {1'b0,  /* decode */
-			  1'b0,  /* encode */
+   wire 	 fo_full   = m_dst_full  || m_dst_almost_full;
+   wire 	 src_empty = m_src_empty || m_src_almost_empty;
+
+   wire [15:0] 	 en_out_data,  de_out_data;
+   wire 	 en_out_valid, de_out_valid;
+   wire 	 en_out_done,  de_out_done;
+   
+   encode encode(.ce(dc[5] && m_enable),
+		 .fi(m_src),
+		 .clk(wb_clk_i),
+		 .rst(wb_rst_i),
+		 .data_o(en_out_data),
+		 .done_o(en_out_done),
+		 .valid_o(en_out_valid),
+		 .m_last(m_src_last),
+		 /*AUTOINST*/
+		 // Outputs
+		 .m_src_getn		(m_src_getn),
+		 // Inputs
+		 .fo_full		(fo_full),
+		 .src_empty		(src_empty));
+   
+   decode decode(.ce(dc[6] && m_enable),
+		 .fi(m_src),
+		 .clk(wb_clk_i),
+		 .rst(wb_rst_i),
+		 .data_o(de_out_data),
+		 .done_o(de_out_done),
+		 .valid_o(de_out_valid),
+		 .m_last(m_src_last),
+		 /*AUTOINST*/
+		 // Outputs
+		 .m_src_getn		(m_src_getn),
+		 // Inputs
+		 .fo_full		(fo_full),
+		 .src_empty		(src_empty));
+
+   codeout codeout (/*AUTOINST*/
+		    // Outputs
+		    .m_dst		(m_dst[63:0]),
+		    .m_dst_putn		(m_dst_putn),
+		    .m_dst_last		(m_dst_last),
+		    .m_endn		(m_endn),
+		    // Inputs
+		    .wb_clk_i		(wb_clk_i),
+		    .wb_rst_i		(wb_rst_i),
+		    .dc			(dc[23:0]),
+		    .en_out_data	(en_out_data[15:0]),
+		    .de_out_data	(de_out_data[15:0]),
+		    .en_out_valid	(en_out_valid),
+		    .de_out_valid	(de_out_valid),
+		    .en_out_done	(en_out_done),
+		    .de_out_done	(de_out_done),
+		    .m_enable		(m_enable));
+
+   assign 	 m_cap = {1'b1,  /* decode */
+			  1'b1,  /* encode */
 			  1'b0,  /* memcpy */
-			  1'b1,  /* rabinpoly */
+			  1'b0, 
 			  1'b0, 
 			  1'b0, 
 			  1'b0};
